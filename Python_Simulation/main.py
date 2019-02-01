f22116585e4adc869fad5ce45e74e6f66e238af3
Chojnacki <alexandre.04@hotmail.fr>
2019-02-01 14:30:57 +0100

    added mock model checkpoints

--- Python_Simulation/main.py
+++ Python_Simulation/main.py
@@ -74,14 +74,14 @@ if __name__ == "__main__":
     main(initial_x)
 
 
-    from threading import Thread
-
-    def func1():
-        print('Working')
-
-    def func2():
-        print('Working')
-
-    if __name__ == '__main__':
-        Thread(target = func1).start()
-        Thread(target = func2).start()
+#    from threading import Thread
+#
+#    def func1():
+#        print('Working')
+#
+#    def func2():
+#        print('Working')
+#
+#    if __name__ == '__main__':
+#        Thread(target = func1).start()
+#        Thread(target = func2).start()
